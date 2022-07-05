6d149c8727c3092e9c0753092c85c5eafb711b15
Will Downey <willd20@vt.edu>
2022-07-05 12:16:04 -0400

    Switch component

---
+++ src/utils/Switch.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Switch = ({
+    onChange,
+    label,
+}) => {
+    return (
+        <div className="flex justify-center">
+        <div className="form-check form-switch">
+            <input onChange={onChange} className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault"></input>
+            <label className="form-check-label inline-block text-gray-800" for="flexSwitchCheckDefault">{label}</label>
+        </div>
+        </div>
+    );
+}
