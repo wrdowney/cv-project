2766c922ba619ad12993d767f5dcbfc7ca128d32
Will Downey <willd20@vt.edu>
2022-07-05 18:57:16 -0400

    Inputs keep value after switching to preview

--- src/components/CVForm/Information.js
+++ src/components/CVForm/Information.js
@@ -5,18 +5,17 @@ const Information = ({mode, handleChange, cv}) => {
     if(!mode) {
         return(
             <div className=" flex flex-col gap-2">
-                <h2 className="font-bold border-b-2">Information</h2>
-                <Input type="text" name="firstName" placeholder="First Name" onChange={(e) => handleChange(e)}/>
-                <Input type="text" name="lastName" placeholder="Last Name" onChange={(e) => handleChange(e)}/>
-                <Input type="text" name="email" placeholder="Email" onChange={(e) => handleChange(e)}/>
-                <Input type="text" name="phone" placeholder="Phone" onChange={(e) => handleChange(e)}/>
-                <Input type="text" name="address" placeholder="Address" onChange={(e) => handleChange(e)}/>
-                <Input type="text" name="description" placeholder="Description" onChange={(e) => handleChange(e)}/>
+                <h2 className="font-bold border-b-2 text-xl">Information</h2>
+                <Input type="text" name="firstName" placeholder="First Name" onChange={(e) => handleChange(e)} value={cv.firstName}/>
+                <Input type="text" name="lastName" placeholder="Last Name" onChange={(e) => handleChange(e)} value={cv.lastName}/>
+                <Input type="text" name="email" placeholder="Email" onChange={(e) => handleChange(e)} value={cv.email} />
+                <Input type="text" name="phone" placeholder="Phone" onChange={(e) => handleChange(e)} value={cv.phone} />
+                <Input type="text" name="address" placeholder="Address" onChange={(e) => handleChange(e)} value={cv.address} />
+                <Input type="text" name="description" placeholder="Description" onChange={(e) => handleChange(e)} value={cv.description} />
             </div>
         );
     }
     else {
-        console.log(cv);
         return(
             <div className="flex p-4 justify-between border-b-2 pb-1">
                 <div className="flex flex-col gap-2">
