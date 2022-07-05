94a0c0e4c75bb5da373613cb9d812daf8b1dad43
Will Downey <willd20@vt.edu>
2022-07-05 19:19:16 -0400

    Change position to absolute

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 left-0 w-full p-3 bg-sky-200 shadow-md">
+        <footer className="absolute bottom-0 left-0 w-full p-3 bg-sky-200 shadow-md">
             <p className="text-center">Made by <a href="https://github.com/wrdowney" className="font-bold">wrdowney</a></p>
         </footer>
     );
