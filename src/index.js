54841a0dd5ca8f5834b052176e6bb4f2b96ee123
Will Downey <willd20@vt.edu>
2022-07-05 18:56:25 -0400

    Make root span the entire height of the page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.getElementById('root').classList.add("h-full");
 root.render(
   <React.StrictMode>
     <App />
