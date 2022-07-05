0e71d991eb49ada1fda910e6fb90b48aa28c7ed1
Will Downey <willd20@vt.edu>
2022-07-05 18:55:49 -0400

    Add background color to page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center bg-slate-100 h-full">
       <Header />
       <Main />
       <Footer />
