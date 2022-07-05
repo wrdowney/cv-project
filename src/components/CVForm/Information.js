911dc7337c412ab15e36c53e8ecbc66908297870
Will Downey <willd20@vt.edu>
2022-07-05 13:40:16 -0400

    Input fields for edit mode

--- src/components/CVForm/Information.js
+++ src/components/CVForm/Information.js
@@ -1,21 +1,24 @@
 import React from "react";
-import Section from "../../utils/Section";
+import Input from "../utils/Input";
 
 const Information = (
     mode,
 ) => {
     if(!mode.mode) {
         return(
-            <Section title="Information123">
-                test123
-            </Section>
+            <div className=" flex flex-col gap-2">
+                <h2 className="font-bold border-b-2">Information</h2>
+                <Input type="text" name="first-name" placeholder="First Name" onChange=""/>
+                <Input type="text" name="last-name" placeholder="Last Name" onChange=""/>
+                <Input type="text" name="email" placeholder="Email" onChange=""/>
+                <Input type="text" name="phone" placeholder="Phone" onChange=""/>
+                <Input type="text" name="address" placeholder="Address" onChange=""/>
+            </div>
         );
     }
     else {
         return(
-            <Section title="Information1">
-                test
-            </Section>
+            <></>
         );
     }
     
