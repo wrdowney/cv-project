eff3dc5be2090f070ca7a0f0a111b3801a2141cc
Will Downey <willd20@vt.edu>
2022-07-05 13:07:51 -0400

    Update tailwind classes

--- src/utils/Switch.js
+++ src/utils/Switch.js
@@ -8,7 +8,7 @@ const Switch = ({
         <div className="flex justify-center">
         <div className="form-check form-switch">
             <label className="form-check-label inline-block text-gray-800" htmlFor="flexSwitchCheckDefault">{label}</label>
-            <input onChange={onChange} className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault56"></input>
+            <input onChange={onChange} className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault56"></input>
         </div>
         </div>
     );
