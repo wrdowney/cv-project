a68574d3f8b8072c952285107e75e02992f949d2
Will Downey <willd20@vt.edu>
2022-07-05 18:05:40 -0400

    Change global font to Times New Roman

--- src/components/CVForm/Information.js
+++ src/components/CVForm/Information.js
@@ -11,14 +11,24 @@ const Information = ({mode, handleChange, cv}) => {
                 <Input type="text" name="email" placeholder="Email" onChange={(e) => handleChange(e)}/>
                 <Input type="text" name="phone" placeholder="Phone" onChange={(e) => handleChange(e)}/>
                 <Input type="text" name="address" placeholder="Address" onChange={(e) => handleChange(e)}/>
+                <Input type="text" name="description" placeholder="Description" onChange={(e) => handleChange(e)}/>
             </div>
         );
     }
     else {
         console.log(cv);
         return(
-            <div>
-                <h1>{cv.firstName}</h1>
+            <div className="flex p-4 justify-between border-b-2 pb-1">
+                <div className="flex flex-col gap-2">
+                    <h1 className="font-bold text-3xl">{cv.firstName}</h1>
+                    <h1 className="font-bold text-3xl">{cv.lastName}</h1>
+                    <p className="bottom-0 text-sm">{cv.description}</p>
+                </div>
+                <div className="flex flex-col gap-4 text-sm text-right mt-7">
+                    <p>{cv.email}</p>
+                    <p>{cv.phone}</p>
+                    <p>{cv.address}</p>
+                </div>
             </div>
         );
     }
