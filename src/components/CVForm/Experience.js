28fd694dc7cf0eecdb61f9bdd66b433e49fe5321
Will Downey <willd20@vt.edu>
2022-07-05 19:19:01 -0400

    Add content

--- src/components/CVForm/Experience.js
+++ src/components/CVForm/Experience.js
@@ -1,12 +1,21 @@
 import React from "react"
 
-const Experience = (
-    mode,
-) => {
-    return (
-        <></>
-    );
-    
+const Experience = ({mode})=> {
+    console.log(mode);
+    if(!mode) {
+        return (
+            <div>
+                <h2 className="font-bold border-b-2 text-xl">Experience</h2>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div>
+                123
+            </div>
+        );
+    }  
 }
 
 export default Experience
