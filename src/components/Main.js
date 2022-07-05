54aaffa39919bc7952f44cbf986eb5ee94b2ad9f
Will Downey <willd20@vt.edu>
2022-07-05 13:06:21 -0400

    Change main to arrow function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,43 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Information from './CVForm/Information';
 import Experience from './CVForm/Experience';
 import Education from './CVForm/Education';
 import Switch from '../utils/Switch';
 
-class Main extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            togglePreview: false,
-        }
-    }
-    changeMode() {
-        this.setState((prevState) => 
-        {   
-            return {
-                togglePreview: !prevState.togglePreview
-            }
-        });
-    }
-    render() {
-        return (
-            <div className="m-4 p-4 h-full shadow-md rounded-md border-2">
-                <Switch 
-                    onChange={() => this.changeMode()}
-                    label="Preview"
-                />
-                <Information 
-                    mode = {this.state.togglePreview}
-                />
-                <Education />
-                <Experience />
-            </div>
-        );
-    }
-
-    
+const Main = () => {
+    const [mode, setMode] = useState(false);
+    return (
+        <div className="m-4 p-4 h-full shadow-md rounded-md border-2">
+            <Switch 
+                onChange={() => { setMode(!mode); }}
+                label="Preview"
+            />
+            <Information 
+                mode = {this.state.togglePreview}
+            />
+            <Education />
+            <Experience />
+        </div>
+    );   
 }
-
-
 
 export default Main;
