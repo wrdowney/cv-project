f579034aa00944e52173283058bf6d7b5782bf84
Will Downey <willd20@vt.edu>
2022-07-05 12:27:13 -0400

    Add preview switch

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Information from './CVForm/Information';
 import Experience from './CVForm/Experience';
 import Education from './CVForm/Education';
+import Switch from '../utils/Switch';
 
 const Main = () => {
     return(
         <div className="m-4 p-4 h-full shadow-md rounded-md border-2">
+            <Switch 
+                onChange={changeMode()}
+                label="Preview"
+            >
+            </Switch>
             <Information />
             <Education />
             <Experience />
         </div>
     );
+}
+
+function changeMode(mode) {
+    return () => {
+    }
 }
 
 export default Main;
--- src/utils/Switch.js
+++ src/utils/Switch.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Switch = ({
-    onChange,
     label,
+    onChange
 }) => {
     return (
         <div className="flex justify-center">
         <div className="form-check form-switch">
-            <input onChange={onChange} className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault"></input>
-            <label className="form-check-label inline-block text-gray-800" for="flexSwitchCheckDefault">{label}</label>
+            <label className="form-check-label inline-block text-gray-800" htmlFor="flexSwitchCheckDefault">{label}</label>
+            <input onChange={onChange} className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault56"></input>
         </div>
         </div>
     );
