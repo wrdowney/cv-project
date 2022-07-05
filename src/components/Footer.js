929214c6df2142c0ebd61e2855c50417b63e87e0
Will Downey <willd20@vt.edu>
2022-07-05 19:11:08 -0400

    Change background color and add shadow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 left-0 w-full p-3 bg-orange-300">
-            <p className="text-center text-white">Made by <a href="https://github.com/wrdowney" className="font-bold">wrdowney</a></p>
+        <footer className="fixed bottom-0 left-0 w-full p-3 bg-sky-200 shadow-md">
+            <p className="text-center">Made by <a href="https://github.com/wrdowney" className="font-bold">wrdowney</a></p>
         </footer>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
     return (
-        <header className="flex w-full p-3 bg-orange-300 justify-center items-center">
-            <h1 className="text-2xl font-bold text-center text-white">CV Application</h1>
+        <header className="flex w-full p-3 justify-center items-center shadow-md bg-sky-200">
+            <h1 className="text-2xl font-bold text-center">CV Application</h1>
         </header>
     );
 }
