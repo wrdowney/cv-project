046f31dc720cb9c4c21b3cbbfff9d205fc77ac7d
Will Downey <willd20@vt.edu>
2022-07-06 08:22:17 -0400

    Add experience popup form

--- src/components/CVForm/Experience.js
+++ src/components/CVForm/Experience.js
@@ -1,13 +1,41 @@
-import React from "react"
+import React, {useState} from "react"
+import Button from "../utils/Button";
+import Input from "../utils/Input";
 
-const Experience = ({mode})=> {
-    console.log(mode);
-    if(!mode) {
+const Experience = ({mode, onChange, cv})=> {
+    const [showAddExperienceForm, setShowAddExperienceForm] = useState(false);
+
+    const toggleAddExperienceForm = () => {
         return (
-            <div>
-                <h2 className="font-bold border-b-2 text-xl">Experience</h2>
-            </div>
+            console.log(showAddExperienceForm),
+            setShowAddExperienceForm(!showAddExperienceForm)
         );
+    }
+    
+    if(!mode) {
+        if(showAddExperienceForm) {
+            return (
+                <div className="flex flex-col gap-2 mb-4">
+                    <h2 className="font-bold border-b-2 text-xl mb-2">Experience</h2>
+                    <Input type="text" name="company" placeholder="Company" onChange={(e) => onChange(e)} value={cv.company}/>
+                    <Input type="text" name="position" placeholder="Position" onChange={(e) => onChange(e)} value={cv.position}/>
+                    <Input type="text" name="description" placeholder="Description" onChange={(e) => onChange(e)} value={cv.description}/>
+                    <Input type="text" name="startDate" placeholder="Start Date" onChange={(e) => onChange(e)} value={cv.startDate}/>
+                    <Input type="text" name="endDate" placeholder="End Date" onChange={(e) => onChange(e)} value={cv.endDate}/>
+                    <Input type="text" name="city" placeholder="City" onChange={(e) => onChange(e)} value={cv.city}/>
+                    <Button type="button" onClick={() => toggleAddExperienceForm()} text="Cancel" name="cancel"/>
+
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <h2 className="font-bold border-b-2 text-xl mb-2">Experience</h2>
+                    <Button type="button" text="+Add Experience" name="btn-add-experience" onClick={() => toggleAddExperienceForm()}/>
+                </div>
+            );
+        }
     }
     else {
         return (
