62ff44f8d4da5bb1b850caafe14026fd55077914
Will Downey <willd20@vt.edu>
2022-07-06 11:31:40 -0400

    Change name to resume generator

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Header = () => {
+const Header = ({previewMode, changePreviewMode}) => {
     return (
         <header className="flex w-full p-3 justify-center items-center shadow-md bg-sky-200">
-            <h1 className="text-2xl font-bold text-center">CV Application</h1>
+            <h1 className="text-2xl font-bold text-center">Resume Generator</h1>
         </header>
     );
 }
