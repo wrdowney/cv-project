9994f8b5121b0bdd4aa457c665e3d5be79dfbbc7
Will Downey <willd20@vt.edu>
2022-07-06 07:40:49 -0400

    Add inner text parameter

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Button = ({type, onClick, value, name}) => {
+const Button = ({type, onClick, text, name}) => {
     return (
         <button 
         className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" 
         type={type} 
         onClick={onClick}
-        value={value}
         name={name}
         >
+            {text}
         </button>
     );
 };
