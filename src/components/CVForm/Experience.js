953400f39dd83a6efb2e4a2ba050c2f0be6179b1
Will Downey <willd20@vt.edu>
2022-07-06 09:27:12 -0400

    Change width to full

--- src/components/CVForm/Experience.js
+++ src/components/CVForm/Experience.js
@@ -17,13 +17,20 @@ const Experience = ({mode, onChange, cv})=> {
             return (
                 <div className="flex flex-col gap-2 mb-4">
                     <h2 className="font-bold border-b-2 text-xl mb-2">Experience</h2>
-                    <Input type="text" name="company" placeholder="Company" onChange={(e) => onChange(e)} value={cv.company}/>
-                    <Input type="text" name="position" placeholder="Position" onChange={(e) => onChange(e)} value={cv.position}/>
-                    <Input type="text" name="description" placeholder="Description" onChange={(e) => onChange(e)} value={cv.description}/>
-                    <Input type="text" name="startDate" placeholder="Start Date" onChange={(e) => onChange(e)} value={cv.startDate}/>
-                    <Input type="text" name="endDate" placeholder="End Date" onChange={(e) => onChange(e)} value={cv.endDate}/>
-                    <Input type="text" name="city" placeholder="City" onChange={(e) => onChange(e)} value={cv.city}/>
-                    <Button type="button" onClick={() => toggleAddExperienceForm()} text="Cancel" name="cancel"/>
+                    <form className="flex flex-col gap-2 mb-4">
+                        <Input type="text" name="company" placeholder="Company" onChange={(e) => onChange(e)} value={cv.company}/>
+                        <Input type="text" name="position" placeholder="Position" onChange={(e) => onChange(e)} value={cv.position}/>
+                        <Input type="text" name="description" placeholder="Description" onChange={(e) => onChange(e)} value={cv.description}/>
+                        <Input type="text" name="startDate" placeholder="Start Date" onChange={(e) => onChange(e)} value={cv.startDate}/>
+                        <Input type="text" name="endDate" placeholder="End Date" onChange={(e) => onChange(e)} value={cv.endDate}/>
+                        <Input type="text" name="city" placeholder="City" onChange={(e) => onChange(e)} value={cv.city}/>
+                        <div className="flex gap-2 w-full">
+                            <Button type="button" onClick={() => toggleAddExperienceForm()} text="Cancel" name="cancel"/>
+                            <Button type="submit" onClick={() => toggleAddExperienceForm()} text="Add" name="add"/>
+                        </div>
+                    </form>
+                    
+                    
 
                 </div>
             );
--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({type, onClick, text, name}) => {
     return (
         <button 
-        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" 
+        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full" 
         type={type} 
         onClick={onClick}
         name={name}
